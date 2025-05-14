82dd74788beb0055c479592f713facdcd8a94797
Rafael <rafa.tec.java@hotmail.com>
2025-05-14 20:03:56 -0300

    add new config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,11 @@
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import tseslint, { plugin } from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'dist', 'node_modules'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
