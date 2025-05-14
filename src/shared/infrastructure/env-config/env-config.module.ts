4adc2d0680dd6983175b69fb12406eca5869c7c4
Rafael <rafa.tec.java@hotmail.com>
2025-05-14 20:33:21 -0300

    add @nestjs/config and update EnvConfigModule and EnvConfigService

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  providers: [EnvConfigService]
+  imports: [ConfigModule.forRoot({
+    isGlobal: false, // ou true se quiser que seja global
+  })],
+  providers: [EnvConfigService],
+  exports: [EnvConfigService], // Exporte se outros módulos precisarem dele
 })
-export class EnvConfigModule {}
+export class EnvConfigModule { }
--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  constructor(private configService: ConfigService) {
+    // Aqui você pode adicionar lógica adicional, se necessário
+    // Exemplo: console.log('EnvConfigService initialized');
+  }
+  getappPort(): number {
+    return Number(this.configService.get<number | undefined>('PORT'));
+  }
+  getNodeEnv(): string {
+    return this.configService.get<string | undefined>('NODE_ENV') ?? 'development';
+  }
+}
