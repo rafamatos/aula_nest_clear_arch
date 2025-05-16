4ec903ff5537a3a9b12f0e1c4750d843ebe14ee4
Rafael <rafa.tec.java@hotmail.com>
2025-05-16 12:53:33 -0300

    feat: refactor UserEntity tests to use UserDataBuilder for test data generation

--- src/users/domain/entities/__teste__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__teste__/unit/user.entity.spec.ts
@@ -1,29 +1,48 @@
-import { faker } from '@faker-js/faker';
 import { UserEntity, UserProps } from '../../user.entity';
+import { UserDataBuilder } from '../../testing/helpers/user-data-build';
 
 describe('UserEntity unit teste', () => {
   let props: UserProps
   let sut: UserEntity
 
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    }
+    props = UserDataBuilder({})
     sut = new UserEntity(props)
   })
 
   it('constructor metodo de teste', () => {
-
-
-
-
     expect(sut.props.name).toEqual(props.name)
     expect(sut.props.email).toEqual(props.email)
     expect(sut.props.password).toEqual(props.password)
     expect(sut.props.createdAt).toBeInstanceOf(Date)
 
   })
+
+  it('name metodo de teste', () => {
+    expect(sut.name).toBeDefined()
+    expect(sut.name).toEqual(props.name)
+    expect(typeof sut.name).toBe('string')
+    expect(sut.name).toEqual(props.name)
+  })
+
+  it('email metodo de teste', () => {
+    expect(sut.email).toBeDefined()
+    expect(sut.email).toEqual(props.email)
+    expect(typeof sut.email).toBe('string')
+    expect(sut.email).toEqual(props.email)
+  })
+
+  it('password metodo de teste', () => {
+    expect(sut.password).toBeDefined()
+    expect(sut.password).toEqual(props.password)
+    expect(typeof sut.password).toBe('string')
+    expect(sut.password).toEqual(props.password)
+  })
+
+  it('createdAt metodo de teste', () => {
+    expect(sut.createdAt).toBeDefined()
+    expect(sut.createdAt).toBeInstanceOf(Date)
+  })
+
 
 })
